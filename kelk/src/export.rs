215dd2ca1c27fd81f8cbeddd5d1a3ace76ee1ecd
Mostafa <mostafa.sedaghat@gmail.com>
2021-09-20 00:10:23 +0800

    Decoding the return value

--- kelk/src/export.rs
+++ kelk/src/export.rs
@@ -20,37 +20,42 @@ extern "C" fn allocate(size: usize) -> u32 {
 
 /// deallocate frees the allocate memory.
 #[no_mangle]
-extern "C" fn deallocate(ptr: u32) {
-}
+extern "C" fn deallocate(ptr: u32) {}
 
 /// TODO
-pub fn do_instantiate<E>(instantiate_fn: &dyn Fn(ContextMut) -> Result<Response, E>) -> u32
-where
-    E: Encode,
-{
+pub fn do_instantiate<E: Encode>(instantiate_fn: &dyn Fn(ContextMut) -> E) -> u32 {
     let mut ctx = make_context();
     instantiate_fn(ctx.as_mut());
     0
 }
 
-/// do_execute should be wrapped in an external "C" export, containing a contract-specific function as arg
+/// TODO: FIXME
+/// do_process_msg should be wrapped in an external "C" export, containing a contract-specific function as arg
 ///
 /// - `M`: message type for request
 /// - `E`: error type for responses
-pub fn do_process_msg<'a, D, E>(
-    process_msg_fn: &dyn Fn(ContextMut, D) -> Result<Response, E>,
+pub fn do_process_msg<'a, D: Decode<'a>, E: Encode>(
+    process_msg_fn: &dyn Fn(ContextMut, D) -> E,
     msg_ptr: *const u8,
     length: u32,
-) -> u32
-where
-    D: Decode<'a>,
-    E: Encode,
-{
+) -> u64 {
     let buf: &[u8] = unsafe { core::slice::from_raw_parts(msg_ptr, length as usize) };
     let msg = minicbor::decode(buf).unwrap(); // TODO: return error
     let mut ctx = make_context();
-    process_msg_fn(ctx.as_mut(), msg);
-    0 // TODO: convert res to ptr??
+    let res = process_msg_fn(ctx.as_mut(), msg);
+
+    result_to_region(res)
+}
+
+fn result_to_region<E: Encode>(res: E) -> u64 {
+    let mut vec = alloc::vec::Vec::new();
+    minicbor::encode(res, &mut vec).unwrap();
+
+    let ptr = vec.as_ptr() as u64;
+    let len = vec.len() as u64;
+
+    core::mem::forget(vec);
+    ptr | (len << 32)
 }
 
 /// Make context instance
