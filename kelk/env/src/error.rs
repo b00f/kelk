4b70bda31fc6c736f5ee5d60b576ddf16f244f4b
Mostafa <mostafa.sedaghat@gmail.com>
2021-11-09 23:27:58 +0800

    Removing unused error type

--- kelk/env/src/error.rs
+++ kelk/env/src/error.rs
@@ -2,8 +2,6 @@
 
 /// Kelk error types
 pub enum KelkError {
-    /// The allocated memory is invalid
-    InvalidMemory,
     /// An issue occurred on writing into storage file
     WriteStorageFailed,
     /// if user try to access out of bound offset
