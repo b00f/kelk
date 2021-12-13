66edb0b3b6fe17fbe5faf228926b4dbc20cc86ba
Mostafa Sedaghat Joo <mostafa.sedaghat@gmail.com>
2021-12-13 12:25:21 +0800

    Capacity for BST (#21)
    * Capacity for BST
    
    * Fixing linting issue

--- kelk/lib/src/collections/bst/header.rs
+++ kelk/lib/src/collections/bst/header.rs
@@ -3,20 +3,20 @@ use core::mem::size_of;
 #[repr(C)]
 pub(super) struct Header {
     pub boom: u32,
-    pub key_size: u16,
-    pub value_size: u16,
-    pub count: u32,
-    pub reserved: u32,
+    pub key_len: u16,
+    pub value_len: u16,
+    pub size: u32,
+    pub capacity: u32,
 }
 
 impl Header {
-    pub fn new<K: Sized, V: Sized>() -> Self {
+    pub fn new<K: Sized, V: Sized>(capacity: u32) -> Self {
         Self {
             boom: 0xb3000000,
-            key_size: size_of::<K>() as u16,
-            value_size: size_of::<V>() as u16,
-            count: 0,
-            reserved: 0,
+            key_len: size_of::<K>() as u16,
+            value_len: size_of::<V>() as u16,
+            size: 0,
+            capacity,
         }
     }
 }
